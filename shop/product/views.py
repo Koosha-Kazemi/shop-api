16e485b1ae6f3ebae61b3b3eee8054e677460252
koosha <kooshakazemig@gmail.com>
2025-03-16 23:22:55 +0330

    feat(views): add CategoryView for managing categories

--- shop/product/views.py
+++ shop/product/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Category
+from .serializer import CategorySerializer
+
+
+class CategoryView(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
