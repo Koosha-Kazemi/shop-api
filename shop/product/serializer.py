11623b5aa429d26d285aac86779750b4ff658359
koosha <kooshakazemig@gmail.com>
2025-03-16 22:10:28 +0330

    feat(serializers): add ProductWriteSerializer and ProductReadSerializer

--- shop/product/serializer.py
+++ shop/product/serializer.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
 
-from .models import Category
+from .models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+
+    
+class ProductWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+
+
+class ProductReadSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Product
+        fields = '__all__'
+        read_only_fields = ('final_price_value',)
