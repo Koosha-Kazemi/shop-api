4736a702511008c5bb15f9a284facc6957954080
koosha <kooshakazemig@gmail.com>
2025-03-16 14:44:41 +0330

    feat: add product app

--- shop/config/settings.py
+++ shop/config/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',    
+    'rest_framework',
+    
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
