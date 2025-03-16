1c5557eb51b280b710a6d7a9e0994605c7e10372
koosha <kooshakazemig@gmail.com>
2025-03-16 16:30:27 +0330

    feat:add category model

--- shop/product/models.py
+++ shop/product/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Represents a category in the system.
+
+    Each category can have a parent category, allowing for hierarchical structures.
+    If a parent category is deleted, the parent field of its children will be set to NULL.
+    """
+    title = models.CharField(max_length=20, verbose_name='Category Title', help_text='Enter the title of the category.')
+    parent = models.ForeignKey('self',
+                                  on_delete=models.SET_NULL,
+                                    blank=True,
+                                    null=True,
+                                     verbose_name='Parent Category',
+                                     help_text='Select a parent category (optional).'
+                                     
+                                      )    
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+
+
+
