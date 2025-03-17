72d9cca0588c1e9efdda59c7eedc249cc9aec805
koosha <kooshakazemig@gmail.com>
2025-03-17 12:23:38 +0330

    feat: Add ProductView with dynamic serializer selection based on request method

--- shop/product/views.py
+++ shop/product/views.py
@@ -1,10 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import SAFE_METHODS
 
-from .models import Category
-from .serializer import CategorySerializer
+from .models import Category, Product
+from .serializer import CategorySerializer, ProductReadSerializer, ProductWriteSerializer
 
 
 class CategoryView(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     
+
+
+
+class ProductView(ModelViewSet):
+    queryset = Product.objects.all()
+
+
+    def get_serializer_class(self):
+        """
+        Use ProductReadSerializer for safe methods (GET, HEAD, OPTIONS),
+        and ProductWriteSerializer for unsafe methods (POST, PUT, PATCH, DELETE).
+        """
+        
+        if self.request.method in SAFE_METHODS:
+            return ProductReadSerializer
+        return ProductWriteSerializer
