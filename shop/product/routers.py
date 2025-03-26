b2a0afb0e32a18ceab194a6224acea01e8c53c72
koosha <kooshakazemig@gmail.com>
2025-03-26 18:16:38 +0330

    feat(routing): add ProductViewSet router

--- shop/product/routers.py
+++ shop/product/routers.py
@@ -1,12 +1,13 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import CategoryViewSet
+from .views import CategoryViewSet, ProductViewSet
 
 
 router = SimpleRouter()
 
 
 router.register(r'category', CategoryViewSet)
+router.register(r'prodct', ProductViewSet)
 
 
 api_router = router.urls
