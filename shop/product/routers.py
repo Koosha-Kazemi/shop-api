1bda88ed4282235cca825aaa1280cee2cc554805
koosha <kooshakazemig@gmail.com>
2025-03-26 18:01:09 +0330

    fix(views): correct CategoryViewSet import in routers.py

--- shop/product/routers.py
+++ shop/product/routers.py
@@ -1,12 +1,12 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import CategoryView
+from .views import CategoryViewSet
 
 
 router = SimpleRouter()
 
 
-router.register(r'category', CategoryView)
+router.register(r'category', CategoryViewSet)
 
 
 api_router = router.urls
