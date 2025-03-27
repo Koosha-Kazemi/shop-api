df32c2e5fd216258ef4e06e39f7c3d0aa6094db1
koosha <kooshakazemig@gmail.com>
2025-03-27 11:43:33 +0330

    feat(pagination): configure global default of 30 items

--- shop/config/settings.py
+++ shop/config/settings.py
@@ -136,5 +136,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 30
+}
+
 
 AUTH_USER_MODEL = 'account.Customer'
