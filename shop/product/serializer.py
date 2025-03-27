41a0a0e0fd20bbaede1210ac321f5b05dbaa8d67
koosha <kooshakazemig@gmail.com>
2025-03-27 11:47:33 +0330

    feat(ImageSerializer): add ImageSerializer

--- shop/product/serializer.py
+++ shop/product/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Category, Product
+from .models import Category, Product, Image
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -9,13 +9,16 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-
-
 class ProductSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Product
         fields = '__all__'
         read_only_fields = ('final_price_value',)
+        
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ('image',)
 
 
