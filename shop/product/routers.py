36a67498977bd621e953b81da11ba2f1340ac0bc
Koosha <kooshakazemig@gmail.com>
2025-04-04 19:09:50 +0330

    feat(router): register API endpoint for option groups

--- shop/product/routers.py
+++ shop/product/routers.py
@@ -1,12 +1,13 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import CategoryViewSet
+from .views import CategoryViewSet, OptionGroupViewSet
 
 
 router = SimpleRouter()
 
 
 router.register(r'category', CategoryViewSet)
+router.register(r'option-group', OptionGroupViewSet)
 
 
 api_router = router.urls
